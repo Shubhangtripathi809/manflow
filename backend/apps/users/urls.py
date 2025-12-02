aa77b377074bfe753ab9afd83bc383f9dace7be4
Harshitlens <harshitshukla@lensvox.com>
2025-12-02 17:30:34 +0530

    Add the role based authentication

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path("register/", views.RegisterView.as_view(), name="register"),
+    path("create-user/", views.UserCreateView.as_view(), name="create-user"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("me/", views.MeView.as_view(), name="me"),
