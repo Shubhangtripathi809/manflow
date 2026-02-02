1a1e7ff97b0e00e7fb9c43ed21d18b46e5bef02f
Harshitlens <harshitshukla@lensvox.com>
2026-02-02 16:28:56 +0530

    change from 1 minute to 60 minute

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -181,7 +181,7 @@ REST_FRAMEWORK = {
 # JWT Settings
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(
-        minutes=config("JWT_ACCESS_TOKEN_LIFETIME_MINUTES", default=1, cast=int)
+        minutes=config("JWT_ACCESS_TOKEN_LIFETIME_MINUTES", default=60, cast=int)
     ),
     "REFRESH_TOKEN_LIFETIME": timedelta(
         days=config("JWT_REFRESH_TOKEN_LIFETIME_DAYS", default=7, cast=int)
